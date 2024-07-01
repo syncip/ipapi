8533a273163e68dcfd7364d7d2ef61b6d887d431
Martin <62094884+syncip@users.noreply.github.com>
2024-07-01 10:58:55 +0200

    Counter

--- index.php
+++ index.php
@@ -7,5 +7,22 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
 
+// Counter
+$counterFile = 'counter.txt';
+
+if (!file_exists($counterFile)) {
+    // Datei erstellen und den initialen Wert auf 0 setzen
+    file_put_contents($counterFile, '0');
+}
+
+// Datei öffnen und den aktuellen Zählerwert lesen
+$counter = (int)file_get_contents($counterFile);
+
+// Zähler um 1 erhöhen
+$counter++;
+
+// Aktualisierten Zählerwert zurück in die Datei schreiben
+file_put_contents($counterFile, (string)$counter);
+
 echo $ip,"\n";
 ?>
